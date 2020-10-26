82f8295defa31e01748355e97fee6c2c7f2b4860
Marit Lund <marit.lund@bouvet.no>
2020-10-26 14:42:10 +0100

    Fix file that was not supposed to be commited

--- api/config/firebase.go
+++ api/config/firebase.go
@@ -5,14 +5,11 @@ import (
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
-
-	"google.golang.org/api/option"
 )
 
 // InitFirebase initialize the firebase connection
 func InitFirebase() *auth.Client {
-	opt := option.WithCredentialsFile("/Users/maritlund/utvikling/DAT250/prosjekt/creds/feedapp-dde0b-firebase-adminsdk-u9xmw-8548a93ada.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		panic("Firebase load error")
 	}
